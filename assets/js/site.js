1d65bb9b3e988c4a33da1e55ba149a242042f1b0
Anna <duncanhaig@btopenworld.com>
2019-05-29 23:40:14 +0100

    close buttons

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,17 +1,28 @@
 
-let tops = document.getElementsByClassName('card-top');
+const tops = document.getElementsByClassName('card-top');
 
 for (const top of tops) {
     if (!top.hasAttribute('link')) continue;
     top.addEventListener('click', (event) => {
-        let target = top.getAttribute('link');
-        let modals = document.getElementsByClassName('modal');
+        const target = top.getAttribute('link');
+        const modals = document.getElementsByClassName('modal');
         for (const modal of modals) {
             if (modal.id == target) {
                 modal.className += " show";
             } else {
-                modal.className = modal.className.replace(" show", '');
+                modal.className = modal.className.replace("show", '').trim();
             }
         }
     });
+}
+
+const closes = document.getElementsByClassName('close');
+
+for (const close of closes) {
+    if (!close.hasAttribute('link')) continue;
+    close.addEventListener('click', (event) => {
+        const target = close.getAttribute('link');
+        const modal = document.getElementById(target);
+        modal.className = modal.className.replace("show", '').trim();
+    }); 
 }
